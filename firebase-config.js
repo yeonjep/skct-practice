950afc542df8971c3936bd31e7f76a4fd854a564
Yeonje Park <yeonje1004@naver.com>
2026-09-03 14:49:28 +0900

    Connect the Firebase web app config.
    구글 로그인과 Firestore를 켤 수 있게 프로젝트 키를 넣는다.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Firebase 웹 설정. 콘솔에서 복사해 붙여 넣으면 구글 로그인이 켜집니다.
-// https://console.firebase.google.com → 프로젝트 만들기 → 웹 앱 추가
+// Firebase 웹 설정. 콘솔의 웹 앱 값입니다. (웹 apiKey는 공개되어도 되고, 보호는 Firestore 규칙이 합니다.)
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDaxmY1d4BHDklCMW314sdmuhKAugBtYWk",
+  authDomain: "skct-practice.firebaseapp.com",
+  projectId: "skct-practice",
+  storageBucket: "skct-practice.firebasestorage.app",
+  messagingSenderId: "566597624067",
+  appId: "1:566597624067:web:f20d146b71c70c28344936",
 };
